b23a6ab81cc47bef9281201c38711e23eb34a706
let rec <996569+letrec@users.noreply.github.com>
2021-12-13 18:53:05 +0800

    Better names

--- src/naive_cache.erl
+++ src/naive_cache.erl
@@ -32,55 +32,58 @@ init(From, F) ->
 
 get({Server, Table}, Key) ->
     case ets:lookup(Table, Key) of
-        [{Key, Value}] ->
-            io:format("Value for ~w is in the cache~n", [Key]),
-            Value;
         [] ->
             io:format("Value for ~w is not in the cache~n", [Key]),
-            Server ! {self(), {get, Key}},
+            Server ! {self(), {eval_key, Key}},
             receive
                 {Server, Value} -> Value
-            end
+            end;
+        [{Key, Value}] ->
+            io:format("Value for ~w is in the cache~n", [Key]),
+            Value
     end.
 
 loop({F, Cached, Pending} = S) ->
     receive
         stop ->
             ok;
-        {From, {get, Key}} ->
+        {From, {eval_key, Key}} ->
             case ets:lookup(Cached, Key) of
-                [{Key, Value}] ->
-                    From ! {self(), Value};
                 [] ->
+                    % TODO: consider using `ets:select_replace` instead of `ets:lookup`
                     case ets:lookup(Pending, Key) of
-                        [{Key, WaiterPids}] ->
-                            true = ets:insert(Pending, {Key, [From | WaiterPids]});
                         [] ->
-                            Server = self(),
-                            spawn(fun() ->
-                                io:format("Evaluating function at ~w~n", [Key]),
-                                try F(Key) of
-                                    Value ->
-                                        io:format("Evaluated ~w to ~w~n", [Key, Value]),
-                                        Server ! {getOk, Key, Value}
-                                catch
-                                    _Class:Reason:_Stacktrace ->
-                                        io:format("Failed to evaluate ~w due to ~w~n", [Key, Reason]),
-                                        Server ! {getError, Key, Reason}
-                                end
-                            end),
-                            true = ets:insert_new(Pending, {Key, [From]})
-                    end
+                            true = ets:insert_new(Pending, {Key, [From]}),
+                            schedule_key_eval(F, Key, self());
+                        [{_Key, WaiterPids}] ->
+                            ets:insert(Pending, {Key, [From | WaiterPids]})
+                    end;
+                [{Key, Value}] ->
+                    From ! {self(), Value}
             end,
             loop(S);
-        {getOk, Key, Value} ->
+        {key_eval_succeeded, Key, Value} ->
             true = ets:insert_new(Cached, {Key, Value}),
             notify_waiting_getters(Pending, Key, Value),
             loop(S);
-        {getError, Key, Reason} ->
+        {key_eval_failed, Key, Reason} ->
             notify_waiting_getters(Pending, Key, Reason),
             loop(S)
     end.
+
+schedule_key_eval(F, Key, ReplyTo) ->
+    _Pid = spawn(fun() ->
+        io:format("Evaluating function at ~w~n", [Key]),
+        try F(Key) of
+            Value ->
+                io:format("Evaluated ~w to ~w~n", [Key, Value]),
+                ReplyTo ! {key_eval_succeeded, Key, Value}
+        catch
+            _Class:Reason:_Stacktrace ->
+                io:format("Failed to evaluate ~w due to ~w~n", [Key, Reason]),
+                ReplyTo ! {key_eval_failed, Key, Reason}
+        end
+    end).
 
 notify_waiting_getters(Pending, Key, Value) ->
     [{_Key, WaiterPids}] = ets:lookup(Pending, Key),
