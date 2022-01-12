7ebafd7f0f7fcae3cb96848cf4c584beda63d370
let rec <996569+letrec@users.noreply.github.com>
2022-01-12 21:23:44 +0800

    Latest changes

--- src/naive_cache.erl
+++ src/naive_cache.erl
@@ -2,6 +2,10 @@
 
 -include_lib("stdlib/include/ms_transform.hrl").
 
+-import(erlang, [
+    monotonic_time/0
+]).
+
 -import(lists, [
     foreach/2,
     reverse/1
@@ -11,8 +15,17 @@
     start/0,
     stop/1,
     init/1,
+    get_async/3,
     get/3
 ]).
+
+-record(pending, {key, pids, counters}).
+-record(done, {key, value, time, counters}).
+
+-define(counters_size, 2).
+-define(hit_ix, 1).
+-define(miss_ix, 2).
+% -define(eval_time_ix, 3).
 
 start() ->
     Pid = spawn(?MODULE, init, [self()]),
@@ -33,8 +46,9 @@ init(From) ->
 
 eval_key(Tid, Fun, Key, Pid) ->
     Eval = fun() ->
+        Start = monotonic_time(),
         try Fun(Key) of
-            Val -> eval_succeeded(Tid, Fun, Key, Val, Pid)
+            Val -> eval_succeeded(Tid, Fun, Key, Val, monotonic_time() - Start, Pid)
         catch
             _:Reason -> eval_failed(Tid, Fun, Key, Reason, Pid)
         end
@@ -42,7 +56,9 @@ eval_key(Tid, Fun, Key, Pid) ->
     spawn(Eval).
 
 register_first_waiter(Tid, Fun, Key, Pid) ->
-    case ets:insert_new(Tid, {Key, wait, [Pid]}) of
+    Counters = counters:new(?counters_size, [write_concurrency]),
+    counters:add(Counters, ?miss_ix, 1),
+    case ets:insert_new(Tid, {Key, wait, [Pid], Counters}) of
         true  -> eval_key(Tid, Fun, Key, Pid);
         false -> register_additional_waiter(Tid, Fun, Key, Pid)
     end.
@@ -50,49 +66,54 @@ register_first_waiter(Tid, Fun, Key, Pid) ->
 register_additional_waiter(Tid, Fun, Key, Pid) ->
     case ets:lookup(Tid, Key) of
         % the value is here and there's no need to wait anymore
-        [{_key, value, Val}] -> Pid ! {ok, Val};
+        [{_key, value, Val, _Time, Counters}] ->
+            counters:add(Counters, ?hit_ix, 1),
+            Pid ! {ok, Val};
         % previous attempt failed, failures are not cached, so we need to try again
         [] -> register_first_waiter(Tid, Fun, Key, Pid);
-        [{_key, wait, Pids}] ->
-            MS = ets:fun2ms(fun({K, wait, Ps}) when K =:= Key, Ps =:= Pids -> {K, wait, [Pid | Ps]} end),
+        [{_key, wait, Pids, Counters}] ->
+            MS = ets:fun2ms(fun({K, wait, Ps, Cs}) when K =:= Key, Ps =:= Pids -> {K, wait, [Pid | Ps], Cs} end),
             case ets:select_replace(Tid, MS) of
-                1 -> ok;
+                1 -> counters:add(Counters, ?miss_ix, 1);
                 0 -> register_additional_waiter(Tid, Fun, Key, Pid)
             end
     end.
 
-eval_succeeded(Tid, Fun, Key, Val, Pid) ->
-    case ets:lookup(Tid, Key) of
-        [{_key, wait, Pids}] ->
-            MS = ets:fun2ms(fun({K, wait, Ps}) when K =:= Key, Ps =:= Pids -> {K, value, Val} end),
-            case ets:select_replace(Tid, MS) of
-                1 -> foreach(fun(P) -> P ! {ok, Val} end, reverse(Pids));
-                0 -> eval_succeeded(Tid, Fun, Key, Val, Pid)
-            end
+eval_succeeded(Tid, Fun, Key, Val, Time, Pid) ->
+    [{_key, wait, Pids, _Counters}] = ets:lookup(Tid, Key),
+    MS = ets:fun2ms(fun({K, wait, Ps, Cs}) when K =:= Key, Ps =:= Pids -> {K, value, Val, Time, Cs} end),
+    case ets:select_replace(Tid, MS) of
+        1 -> foreach(fun(P) -> P ! {ok, Val} end, reverse(Pids));
+        0 -> eval_succeeded(Tid, Fun, Key, Val, Time, Pid)
     end.
 
 eval_failed(Tid, Fun, Key, Reason, Pid) ->
+    [{_key, wait, Pids, _Counters}] = ets:lookup(Tid, Key),
+    MS = ets:fun2ms(fun({K, wait, Ps, _Cs}) -> K =:= Key andalso Ps =:= Pids end),
+    case ets:select_delete(Tid, MS) of
+        1 -> foreach(fun(P) -> P ! {error, Reason} end, reverse(Pids));
+        0 -> eval_failed(Tid, Fun, Key, Reason, Pid)
+    end.
+
+-spec get_async(ets:tid(), fun((any()) -> any()), any()) -> {ok, any()} | pending.
+get_async(Tid, Fun, Key) ->
     case ets:lookup(Tid, Key) of
-        [{_key, wait, Pids}] ->
-            MS = ets:fun2ms(fun({K, wait, Ps}) -> K =:= Key andalso Ps =:= Pids end),
-            case ets:select_delete(Tid, MS) of
-                1 -> foreach(fun(P) -> P ! {error, Reason} end, reverse(Pids));
-                0 -> eval_failed(Tid, Fun, Key, Reason, Pid)
-            end
+        [{_key, value, Value, _Time, Counters}] ->
+            counters:add(Counters, ?hit_ix, 1),
+            {ok, Value};
+        [] ->
+            register_first_waiter(Tid, Fun, Key, self()),
+            pending;
+        [{_key, wait, _pids}] ->
+            register_additional_waiter(Tid, Fun, Key, self()),
+            pending
     end.
 
 get(Tid, Fun, Key) ->
-    case ets:lookup(Tid, Key) of
-        [{_key, value, Value}] ->
+    case get_async(Tid, Fun, Key) of
+        {ok, Value} ->
             Value;
-        [] ->
-            register_first_waiter(Tid, Fun, Key, self()),
-            receive
-                {ok, Value}     -> Value;
-                {error, Reason} -> throw(Reason)
-            end;
-        [{_key, wait, _pids}] ->
-            register_additional_waiter(Tid, Fun, Key, self()),
+        pending ->
             receive
                 {ok, Value}     -> Value;
                 {error, Reason} -> throw(Reason)
--- test/naive_cache_SUITE.erl
+++ test/naive_cache_SUITE.erl
@@ -8,6 +8,10 @@
 id(X) ->
     X.
 
+sleep_id(X) ->
+    timer:sleep(100),
+    X.
+
 div_by_zero(X) ->
     X / 0.
 
@@ -17,9 +21,23 @@ start_stop_test() ->
 
 get_test() ->
     {Pid, Tid} = naive_cache:start(),
-    naive_cache:get(Tid, fun(X) -> X end, 42),
     ?assertEqual(42, naive_cache:get(Tid, fun id/1, 42)),
     ?assertEqual(42, naive_cache:get(Tid, fun id/1, 42)),
+    ok = naive_cache:stop(Pid).
+
+get2_test() ->
+    {Pid, Tid} = naive_cache:start(),
+    ?assertEqual(pending, naive_cache:get_async(Tid, fun sleep_id/1, 42)),
+    ?assertEqual(pending, naive_cache:get_async(Tid, fun sleep_id/1, 42)),
+    receive
+        {ok, 42} -> ?assert(true);
+        _ -> ?assert(false)
+    end,
+    receive
+        {ok, 42} -> ?assert(true);
+        _ -> ?assert(false)
+    end,
+
     ok = naive_cache:stop(Pid).
 
 get_failed_test() ->
